110f2ee062fb16d53f19f8aae987bc3fbd68f1db
Fuadain <nicholausafleming@gmail.com>
2023-02-28 15:13:47 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route}
+    from 'react-router-dom';
+import Navbar from "./navbar"
+import Home from "./pages/home/home"
+import Quiz from "./pages/quiz/quiz"
+import QuizGen from "./pages/quiz-gen/quiz-gen"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route exact path='/' element={<Home/>} />
+          <Route path='/quiz' element={<Quiz/>} />
+          <Route path='/quiz-gen' element={<QuizGen/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
