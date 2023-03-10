05aed01419722181fe19aac6c8dca53a82231c4b
Fuadain <nicholausafleming@gmail.com>
2023-03-10 15:54:15 -0800

    added react-select library and used in quiz-action

--- src/pages/quiz-action/quiz-action.js
+++ src/pages/quiz-action/quiz-action.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './quiz-action.css';
 import QuizTime from './quiz-time'
+import Select from 'react-select';
 
 export default function QuizAction(){
     const [inputData, setInputData] = React.useState({
@@ -8,15 +9,16 @@ export default function QuizAction(){
         startDate:null,
         endDate: null
     })
-    const [classesElements, setClassesElements] = React.useState()
+    const [classOptions, setClassOptions] = React.useState()
 
     React.useEffect(()=>{
-        setClassesElements(()=>{
-            
-            const instructorsClasses = [1,2,3] //api class jargon
-            const newElements = instructorsClasses.map(element=><option key={element} value={element}>{element}</option>)
-
-            return newElements
+        setClassOptions(()=>{
+            const newClasses = [
+                {label:"1", value:"1"},
+                {label:"2", value:"2"},
+                {label:"3", value:"3"}
+            ] //api class jargon
+            return newClasses
         })
     },[])
 
@@ -31,6 +33,7 @@ export default function QuizAction(){
         //quiz assign backend jargon
     }
 
+    //Keeping this here to deal with any addition inputs that don't need a library
     function inputChange(event){
         const input = event.target
         setInputData(prevData=>{
@@ -48,15 +51,31 @@ export default function QuizAction(){
         })
     }
 
+    function changeSelectedClasses(selection){
+        setInputData(prevData=>{
+            return {
+                ...prevData,
+                classes: selection
+            }
+        })
+    }
+
     // for assigning to multiple classes, need something better than select
     console.log(inputData.startDate)
     return (
         <form onSubmit={assignQuiz}>
             <button type="button" onClick={printQuiz}>Print Out</button>
             <label>Assign Test To Class</label>
-            <select name="classes" required multiple value={inputData.selectedClasses} onChange={inputChange}>
-                {classesElements}
-            </select>
+            <Select
+                isMulti
+                name="classes"
+                options={classOptions}
+                className="basic-multi-select"
+                classNamePrefix="select"
+                onChange={changeSelectedClasses}
+                defaultValue={inputData.classes}
+                required
+            />
             <QuizTime startDate={inputData.startDate} endDate={inputData.endDate} changeDate={changeDate}/>
             <button type="submit">Assign Quiz</button>
         </form>
