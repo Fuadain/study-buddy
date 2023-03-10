22436cce18dca7486b8b855b81615d194fa737da
Lawrence-Clemente <lawrence.clemente.cohort225@gmail.com>
2023-03-10 15:27:00 -0800

    changed anchor tags to NavLink

--- src/navbar/navbar.js
+++ src/navbar/navbar.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './navbar.css';
+import { NavLink } from 'react-router-dom';
 
 export default function Navbar(){
     return (
         <nav id="navbar">
-               <a href="#" id="logo"><i class="fa-solid fa-school fa-xl"></i></a>
+               <NavLink to="/dashboard" id="logo"><FontAwesomeIcon icon="fa-solid fa-school" /></NavLink>
         </nav>
     )
 }
--- src/sidebar/sidebar.js
+++ src/sidebar/sidebar.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './sidebar.css';
+import { NavLink } from 'react-router-dom';
 
 export default function Sidebar(){
     return (
         <nav id="sidebar">
                 <ul>
                     <li><h2 id="sidebar-welcome">Welcome Student!</h2></li>
-                    <li><a href="#" id="sidebar-profile" class="sidebar-option">Profile</a></li>
-                    <li><a href="#" id="sidebar-logout" class="sidebar-option">Logout</a></li>
+                    <li><NavLink to="/dashboard" id="sidebar-profile" className="sidebar-option" activeClassName="active">Home</NavLink></li>
+                    <li><NavLink to="/profile" id="sidebar-profile" className="sidebar-option" activeClassName="active">Profile</NavLink></li>
+                    <li><NavLink to="/logout" id="sidebar-logout" className="sidebar-option" activeClassName="active">Logout</NavLink></li>
                 </ul>
         </nav>
     )
