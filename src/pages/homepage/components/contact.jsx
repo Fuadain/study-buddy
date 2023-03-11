eb407b58d97a8e5c491e32368ffc9fe437d0e489
alex-labra <alex.labra.94@gmail.com>
2023-03-10 16:02:21 -0800

    save

--- src/pages/homepage/components/contact.jsx
+++ src/pages/homepage/components/contact.jsx
@@ -1,17 +1,93 @@
 import React from 'react';
-import { Stack, Button } from '@mui/material';
+import { Stack, Button, Box, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const Contact = () => {
   return (
     <Stack
-        direction="row"
-        alignItems="center"
-        p={2}
-        sx={{ position: 'sticky', background: 'gray', top: 0, justifyContent: 'flex-end' }}
+      display='flex'
+      alignItems='center'
+      justifyContent='center'
+      sx={{
+        flexDirection: { sx: "column", md: "row" }
+      }}
     >
 
-        <Button variant="contained" sx={{ marginRight: '20px' }}>login</Button>
-        <Button variant="contained">Sign Up</Button>
+
+      <Box
+        display='flex'
+        alignItems='center'
+        justifyItems='center'
+        sx={{
+          height: '90vh',
+          backgroundColor: '#00B2FF',
+          borderRadius: '0.5em'
+        }}
+      >
+        <Box>
+          <Typography
+            sx={{
+              color: '#000',
+              backgroundColor: '#00B2FF',
+
+            }}
+            variant='h3'
+            padding={2}
+          >
+            GET IN TOUCH WITH US!
+          </Typography>
+          <Typography
+            sx={{
+              color: '#000',
+              backgroundColor: '#00B2FF',
+            }}
+            variant='body1'
+            padding={2}
+          >
+            Whether article spirits new her covered hastily sitting her. Money witty <br />
+            incididunt ut labore et dolore magna aliqua. <br /><br />
+            +1 (321)-321-3210<br /><br />
+            info@bayvalleytech.com<br /><br />
+            545, Street 11, Block F<br />
+            Modesto, California<br />
+            <Link
+              to='/'
+            >
+              <Button
+                variant="contained"
+                sx={{
+                  color: 'black',
+                  backgroundColor: 'red',
+                  mt: '20px'
+                }}
+              >
+                BACK TO HOMEPAGE
+              </Button>
+            </Link>
+          </Typography>
+
+        </Box>
+
+      </Box>
+      <Box
+        display='flex'
+        alignItems='center'
+        justifyItems='center'
+        sx={{
+          height: '100vh',
+          backgroundColor: 'none'
+        }}
+      >
+        <img
+          src={require('../logo.png')}
+          alt={'logo'}
+          style={{
+            height: '500px',
+            margin: '20px'
+          }}
+
+        />
+      </Box>
 
     </Stack>
   )
