ef80ba6617c582b856b7abb2355d3d9d546b5a85
Connie Tsang <connietsang789@gmail.com>
2023-03-13 18:31:31 -0400

    Add Forgot Password page + route it

---
+++ src/pages/login/forgot-password.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+  Stack, Typography, Button, Box,
+  FormControl, InputAdornment, OutlinedInput,
+  InputLabel, IconButton, FormLabel, RadioGroup, FormHelperText,
+  FormControlLabel, Radio
+} from '@mui/material';
+
+export default function ForgotPassword() {
+  return (
+    <Typography variant="h1" gutterBottom>Forgot Password?</Typography>
+  );
+}
