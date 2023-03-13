ce8735e177be0e0fe1916ac07a63279aa9049a23
Fuadain <nicholausafleming@gmail.com>
2023-03-13 16:31:31 -0700

    beginning styling and fixed end time before start time bug

--- src/pages/quiz-action/quiz-action.js
+++ src/pages/quiz-action/quiz-action.js
@@ -2,6 +2,10 @@ import React from 'react';
 import './quiz-action.css';
 import QuizTime from './quiz-time'
 import Select from 'react-select';
+import ClassSelectStyle from './class-select-style';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import Container from '@mui/material/Container';
 
 export default function QuizAction(){
     const [inputData, setInputData] = React.useState({
@@ -34,6 +38,7 @@ export default function QuizAction(){
     }
 
     //Keeping this here to deal with any addition inputs that don't need a library
+    /*
     function inputChange(event){
         const input = event.target
         setInputData(prevData=>{
@@ -41,6 +46,7 @@ export default function QuizAction(){
             return newData
         })
     }
+    */
 
     function changeDate(date, name){
         setInputData(prevData => {
@@ -63,25 +69,40 @@ export default function QuizAction(){
     // for assigning to multiple classes, need something better than select
     console.log(inputData.startDate)
     return (
-        <form onSubmit={assignQuiz}>
-            <button type="button" onClick={printQuiz}>Print Out</button>
-            <label>Assign Test To Class</label>
-            <Select
-                isMulti
-                name="classes"
-                options={classOptions}
-                className="basic-multi-select"
-                classNamePrefix="select"
-                onChange={changeSelectedClasses}
-                defaultValue={inputData.classes}
-                required
-            />
-            <QuizTime startDate={inputData.startDate} endDate={inputData.endDate} changeDate={changeDate}/>
-            <button type="submit">Assign Quiz</button>
-        </form>
+        <Container maxWidth="sm">
+            <Stack spacing={2}>
+                <Button
+                    variant="contained" 
+                    onClick={printQuiz}
+                    sx={{width:"50%", alignSelf: "center"}}
+                >
+                    Print Out
+                </Button>
+                <div>
+                    <label>Assign Test To Class</label>
+                    <Select
+                        isMulti
+                        name="classes"
+                        options={classOptions}
+                        className="basic-multi-select"
+                        classNamePrefix="select"
+                        onChange={changeSelectedClasses}
+                        defaultValue={inputData.classes}
+                        styles={ClassSelectStyle}
+                        required
+                    />
+                </div>
+                <QuizTime startDate={inputData.startDate} endDate={inputData.endDate} changeDate={changeDate}/>
+                <Button 
+                    variant="contained" 
+                    onClick={assignQuiz}
+                    sx={{width:"50%", alignSelf: "center"}}
+                >
+                    Assign Quiz
+                </Button>
+            </Stack>
+        </Container>
     )
 }
 
-//old way, but needs functionality like range, preventing past dates, etc
-//<input type="date" name="startDate" value={inputData.startDate} required onChange={inputChange}/>
-//<input type="date" name="dueDate" value={inputData.dueDate} required onChange={inputChange}/>
+//react-select library at https://react-select.com/home
--- src/pages/quiz-action/quiz-time.js
+++ src/pages/quiz-action/quiz-time.js
@@ -1,13 +1,18 @@
 import React from "react"
 import DatePicker from "react-datepicker"
 import "react-datepicker/dist/react-datepicker.css";
+import Stack from '@mui/material/Stack';
 
 export default function QuizTime(props){
     
-      const filterPassedTime = (time) => {
-        const currentDate = new Date();
+      const filterPassedTime = (time, isEnd) => {
+        let currentDate 
         const selectedDate = new Date(time);
-    
+        if(!isEnd){
+          currentDate = new Date();
+        } else {
+          currentDate = props.startDate
+        }
         return currentDate.getTime() < selectedDate.getTime();
       }
       
@@ -21,31 +26,36 @@ export default function QuizTime(props){
       console.log(props.startDate)
     
       return (
-        <div>
-          <DatePicker 
-            selected={props.startDate} 
-            onChange={(date)=>props.changeDate(date, "startDate")}
-            selectsStart
-            startDate={props.startDate}
-            endDate={props.endDate}
-            filterTime={filterPassedTime}
-            filterDate={(date)=>filterPassedDays(date)}
-            showTimeSelect
-            dateFormat="MMMM d, yyyy h:mm aa"
-          />
-    
-          <DatePicker 
-            selected={props.endDate}
-            onChange={(date)=>props.changeDate(date, "endDate")}
-            selectsEnd
-            startDate={props.startDate}
-            endDate={props.endDate}
-            minDate={props.startDate}
-            filterTime={filterPassedTime}
-            filterDate={(date)=>filterPassedDays(date)}
-            showTimeSelect
-            dateFormat="MMMM d, yyyy h:mm aa"
-          />
-        </div>
+        <Stack direction="row" spacing={2}>
+          <div>
+            <label>Start Time</label>
+            <DatePicker 
+              selected={props.startDate} 
+              onChange={(date)=>props.changeDate(date, "startDate")}
+              selectsStart
+              startDate={props.startDate}
+              endDate={props.endDate}
+              filterTime={(time)=>filterPassedTime(time, false)}
+              filterDate={(date)=>filterPassedDays(date)}
+              showTimeSelect
+              dateFormat="MMMM d, yyyy h:mm aa"
+            />
+          </div>
+          <div>
+            <label>End Time</label>
+            <DatePicker 
+              selected={props.endDate}
+              onChange={(date)=>props.changeDate(date, "endDate")}
+              selectsEnd
+              startDate={props.startDate}
+              endDate={props.endDate}
+              minDate={props.startDate}
+              filterTime={(time)=>filterPassedTime(time, true)}
+              filterDate={(date)=>filterPassedDays(date)}
+              showTimeSelect
+              dateFormat="MMMM d, yyyy h:mm aa"
+            />
+          </div>
+        </Stack>
       )
 }
