ee072c56386ed6284c2d7c43f31178ce8ec16bed
Lawrence-Clemente <lawrence.clemente.cohort225@gmail.com>
2023-03-15 13:15:29 -0700

    sidebar has logic to display teacher or student info

--- src/navbar/navbar.js
+++ src/navbar/navbar.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './navbar.css';
-import { Link } from 'react-router-dom';
 
 export default function Navbar(){
     return (
-        <div>
-        <nav id="navbar">
-               <Link to="/dashboard" id="logo">Logo Here</Link>
-        </nav>
+        <div id="navbar-container">
+            <div id="navbar">
+                <span>Dashboard</span>
+            </div>
         </div>
     )
 }
--- src/sidebar/sidebar.js
+++ src/sidebar/sidebar.js
@@ -3,16 +3,37 @@ import './sidebar.css';
 import { NavLink } from 'react-router-dom';
 
 export default function Sidebar(){
+    const [isTeacher, identifyUser] = React.useState(false);
+    const [firstName, inputFirstName] = React.useState("Student")
+
+    React.useEffect(() => {
+        isTeacher ? inputFirstName('Teacher') : inputFirstName('Student');
+      }, [isTeacher]);
+
+    function studentSidebar(){
+        return (
+            <ul>
+            <li><h2 id="sidebar-welcome">Welcome {firstName}!</h2></li>
+            <li><NavLink to="/dashboard" id="sidebar-profile" className="sidebar-option" activeClassName="active">Home</NavLink></li>
+            <li><NavLink to="/profile" id="sidebar-profile" className="sidebar-option" activeClassName="active">Profile</NavLink></li>
+            <li><NavLink to="/logout" id="sidebar-logout" className="sidebar-option" activeClassName="active">Logout</NavLink></li>
+        </ul>
+        )
+    }
+
+    function teacherSidebar(){
+        return (
+            <ul>
+            <li><h2 id="sidebar-welcome">Welcome {firstName}!</h2></li>
+            <li><NavLink to="/dashboard" id="sidebar-profile" className="sidebar-option" activeClassName="active">Home</NavLink></li>
+            <li><NavLink to="/profile" id="sidebar-profile" className="sidebar-option" activeClassName="active">Profile</NavLink></li>
+            <li><NavLink to="/logout" id="sidebar-logout" className="sidebar-option" activeClassName="active">Logout</NavLink></li>
+        </ul>
+        )
+    }
     return (
-        <div>
         <nav id="sidebar">
-                <ul>
-                    <li><h2 id="sidebar-welcome">Welcome Student!</h2></li>
-                    <li><NavLink to="/dashboard" id="sidebar-profile" className="sidebar-option" activeClassName="active">Home</NavLink></li>
-                    <li><NavLink to="/profile" id="sidebar-profile" className="sidebar-option" activeClassName="active">Profile</NavLink></li>
-                    <li><NavLink to="/logout" id="sidebar-logout" className="sidebar-option" activeClassName="active">Logout</NavLink></li>
-                </ul>
+        {isTeacher ? teacherSidebar() : studentSidebar()}
         </nav>
-        </div>
     )
 }
