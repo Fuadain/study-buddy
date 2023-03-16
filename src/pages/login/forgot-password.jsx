2a4cf8f76060ccc55cb48401ca7080e2c1fef685
Connie Tsang <connietsang789@gmail.com>
2023-03-16 16:11:35 -0400

    forgot password page

--- src/pages/login/forgot-password.jsx
+++ src/pages/login/forgot-password.jsx
@@ -8,6 +8,36 @@ import {
 
 export default function ForgotPassword() {
   return (
-    <Typography variant="h1" gutterBottom>Forgot Password?</Typography>
-  );
+    <div class="container">
+      <Stack
+        sx={{
+          width: '50%',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          p: 2
+        }}
+      >
+        <Typography variant="h2" gutterBottom>Forgot Password?</Typography>
+        <Typography variant="h5" gutterBottom>Enter your email to reset your password. </Typography>
+
+        <form>
+          <Stack
+          >
+            <FormControl sx={{ m: 1, width: '50ch', maxWidth: '100%' }}>
+              <InputLabel htmlFor="component-outlined">Email</InputLabel>
+              <OutlinedInput
+                fullWidth
+                id="component-outlined fullWidth"
+                label="email"
+              />
+            </FormControl>
+            <Button sx={{ width: '25ch', alignSelf: 'center' }} type="submit" variant="outlined" size="medium">Continue</Button>
+          </Stack>
+        </form>
+      </Stack>
+
+    </div>
+  )
 }
