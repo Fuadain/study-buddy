8b23a44f9d21ef4a355300286de5fb9959fb635c
Omar Ruedas <68258056+omarruedas@users.noreply.github.com>
2023-03-20 15:37:40 -0700

    Add terms.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import { Homepage, Login, ForgotPassword, ForgotPasswordVerify, Registration, Dashboard, 
-  Quiz, QuizGen, QuizAction, AccountPage, Contact, LearnMore } from './';
+import {
+  Homepage,
+  Login,
+  ForgotPassword,
+  Registration,
+  Dashboard,
+  Quiz,
+  QuizGen,
+  QuizAction,
+  AccountPage,
+  Contact,
+  LearnMore,
+  Terms,
+} from "./";
 
 function App() {
   return (
     <div>
       <Router>
         <Routes>
-          <Route exact path='/' element={<Homepage/>} />
-          <Route path='/login' element={<Login/>} />
-          <Route path='/forgot-password' element={<ForgotPassword/>} />
-          <Route path='/forgot-password-verify' element={<ForgotPasswordVerify/>} />
-          <Route path='/registration' element={<Registration/>} />
-          <Route path='/dashboard' element={<Dashboard/>} />
-          <Route path='/quiz' element={<Quiz/>} />
-          <Route path='/quiz-gen' element={<QuizGen/>} />
-          <Route path='/quiz-action' element={<QuizAction/>} />
-          <Route path='/account' element={<AccountPage/>} />
+          <Route exact path="/" element={<Homepage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/registration" element={<Registration />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/quiz" element={<Quiz />} />
+          <Route path="/quiz-gen" element={<QuizGen />} />
+          <Route path="/quiz-action" element={<QuizAction />} />
+          <Route path="/account" element={<AccountPage />} />
 
-          <Route path='/contact' element={<Contact/>} />
-          <Route path='/learnMore' element={<LearnMore/>} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/learnMore" element={<LearnMore />} />
+          <Route path="/terms" element={<Terms />} />
         </Routes>
       </Router>
     </div>
