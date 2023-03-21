2c88222156841b0870e85d54d11effdc351270b0
Omar Ruedas <68258056+omarruedas@users.noreply.github.com>
2023-03-21 15:48:15 -0700

    Update terms.js

--- src/pages/registration/terms.js
+++ src/pages/registration/terms.js
@@ -1,3 +1,45 @@
 export default function Terms() {
-  return <div>terms</div>;
+  return (
+    <>
+      <h1
+        style={{
+          margin: "10px",
+          padding: "10px",
+          alignItems: "center",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        Terms of Service
+      </h1>
+      <p style={{ margin: "10px", color: "black" }}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam cupiditate
+        quibusdam, accusantium ea illum asperiores tempore ipsa nisi quisquam
+        animi perspiciatis facere unde fuga. Omnis, officia eveniet? Error
+        deleniti praesentium, ex repellat tenetur labore accusantium tempora
+        voluptatem eum quibusdam, consequuntur dignissimos ducimus
+        necessitatibus impedit quae sint molestiae minus nostrum veritatis
+        distinctio minima eligendi explicabo soluta. Corporis inventore quas
+        repellat placeat! Libero, impedit itaque? Facilis vitae commodi
+        accusamus alias voluptas! Eius possimus corrupti laborum fugit quam
+        architecto dolores laudantium itaque exercitationem veniam, id deleniti
+        aspernatur minus tempora vero, impedit esse voluptas est alias ex
+        eveniet. Accusantium libero deserunt odio commodi perspiciatis.
+      </p>
+      <p style={{ margin: "10px", color: "black" }}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam cupiditate
+        quibusdam, accusantium ea illum asperiores tempore ipsa nisi quisquam
+        animi perspiciatis facere unde fuga. Omnis, officia eveniet? Error
+        deleniti praesentium, ex repellat tenetur labore accusantium tempora
+        voluptatem eum quibusdam, consequuntur dignissimos ducimus
+        necessitatibus impedit quae sint molestiae minus nostrum veritatis
+        distinctio minima eligendi explicabo soluta. Corporis inventore quas
+        repellat placeat! Libero, impedit itaque? Facilis vitae commodi
+        accusamus alias voluptas! Eius possimus corrupti laborum fugit quam
+        architecto dolores laudantium itaque exercitationem veniam, id deleniti
+        aspernatur minus tempora vero, impedit esse voluptas est alias ex
+        eveniet. Accusantium libero deserunt odio commodi perspiciatis.
+      </p>
+    </>
+  );
 }
