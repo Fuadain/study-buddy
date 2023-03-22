75ad6ccd64988b209c84f8b410053652048fb374
Fuadain <nicholausafleming@gmail.com>
2023-03-22 14:54:42 -0700

    reorganized classboard to use navbar and sidebar

--- src/pages/dashboard/classboard/classboard.js
+++ src/pages/dashboard/classboard/classboard.js
@@ -1,41 +1,48 @@
 import React from 'react';
 import './classboard.css';
-import Header from './components/Header';
-import Class from './components/Class';
+import Navbar from '../../../navbar/navbar'
+import Sidebar from '../../../sidebar/sidebar'
 import Quiz from './components/Quiz';
 import Student from './components/Student';
 import { Quizdata, Studentdata} from './mock-data';
+import Stack from '@mui/material/Stack'
 
 export default function Dashboard(){
-        const quizzes = Quizdata.map(item => {
-            return (
-                < Quiz
-                    key={item.id}
-                    item={item}
-                />
-            )
-        })
+    const pageName = "Class Name"
+    const quizzes = Quizdata.map(item => {
+        return (
+            < Quiz
+                key={item.id}
+                item={item}
+            />
+        )
+    })
 
-        const students = Studentdata.map(item => {
-            return (
-                < Student
-                    key={item.id}
-                    item={item}
-                />
-            )
-        }) 
+    const students = Studentdata.map(item => {
+        return (
+            < Student
+                key={item.id}
+                item={item}
+            />
+        )
+    }) 
 
     return (
-        <div className="class-container">
-            <Header />
-            <Class />
-            <section className="quiz-container">
-                {quizzes}
-            </section>
-            <h2 className="student-title">Students</h2>
-            <section className="student-container">
-                {students}
-            </section>
+        <div>
+            <Navbar pageName={pageName}/>
+            <Sidebar />
+            <Stack spacing={3} sx={{ml: '25vw', mr: '5vw', pt: '2vw'}}>
+                <div className="quiz-title">
+                    <h3>Available Quizzes</h3>
+                </div> 
+                <section className="quiz-container">
+                    {quizzes}
+                </section>
+                <h2 className="student-title">Students</h2>
+                <section className="student-container">
+                    {students}
+                </section>
+            </Stack>
         </div>
     )
 }
--- src/pages/dashboard/classboard/components/Quiz.js
+++ src/pages/dashboard/classboard/components/Quiz.js
@@ -3,11 +3,11 @@ import React from 'react';
 export default function Quiz(props){
     return (
         <div className="quiz-card">
-            <div className="quiz-one">
+            <div className="quiz-section-one">
                 <h2>{props.item.number}</h2>
                 <h4>Questions {props.item.questions}</h4>
             </div>
-            <div className="quiz-two">
+            <div className="quiz-section-two">
                 <h4>Due by: {props.item.date}</h4>
                 <button className="quiz-button">Start</button>
                 <h4>Time Limit: {props.item.time}</h4>
