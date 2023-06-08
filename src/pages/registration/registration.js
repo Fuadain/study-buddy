d4038f34e570be704d63b457b0f53b317d708eb0
Fuadain <nicholausafleming@gmail.com>
2023-06-08 15:13:04 -0700

    prep registration for axios endpoint

--- src/pages/registration/registration.js
+++ src/pages/registration/registration.js
@@ -1,35 +1,54 @@
 
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import "./registration.css";
+import AxiosContext from "../../components/axios-context";
+import axios from 'axios'
+import {Stack} from '@mui/material'
 
 const Register = () => {
   const [inputs, setInputs] = useState({
     email: "",
-    firstname: "",
-    lastname: "",
+    firstName: "",
+    lastName: "",
     password: "",
-    confirm_password: "",
+    confirmPassword: "",
     type: "",
-  });
+  })
+  const {hostname} = useContext(AxiosContext)
 
   const handleChange = (e) => {
     e.preventDefault();
     setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
 
-  /* 
-  placeholder post request to server
+  function submitData(){
+  if(inputs.email && inputs.firstName && inputs.lastName && inputs.password 
+  && inputs.confirmPassword && inputs.type && emailCheck(inputs.email) &&
+  passwordCheck(inputs.password) && inputs.password === inputs.confirmPassword){
+    axios.post(`${hostname}/newUser`, {
+      firstName: inputs.firstName,
+      lastName: inputs.lastName,
+      email: inputs.email,
+      password: inputs.password,
+      type: inputs.type
+    })
+    .then(res=>{
+      //check if email already in use
+      //alert("Email already in use")
+    })
+  } else {
+    alert("Complete form before submitting")
+  }
+}
+  let message = ""
+  if(inputs.password && !passwordCheck(inputs.password))
+    message = "Password have at least 8 characters and 1 number"
+  else if(inputs.confirmPassword && inputs.password !== inputs.confirmPassword)
+    message = "Password does not match"
+  else if (inputs.email && !emailCheck(inputs.email))
+    message = "Email is invalid"
 
-  axios.post('/registration', {
-    username: inputs.username,
-    email: inputs.email,
-    password: inputs.password,
-    type: inputs.type
-  })
-  */
-
-  console.log(inputs);
   return (
     <>
       <div className="register">
@@ -45,38 +64,43 @@ const Register = () => {
             name="email"
             onChange={handleChange}
           />
-          <input
-            className="registration-input"
-            required
-            type="text"
-            placeholder="First Name"
-            name="firstname"
-            onChange={handleChange}
-          />
-          <input
-            className="registration-input"
-            required
-            type="text"
-            placeholder="Last Name"
-            name="lastname"
-            onChange={handleChange}
-          />
-          <input
-            className="registration-input"
-            required
-            type="password"
-            placeholder="Password"
-            name="password"
-            onChange={handleChange}
-          />
-          <input
-            className="registration-input"
-            required
-            type="password"
-            placeholder="Confirm Password"
-            name="confirm_password"
-            onChange={handleChange}
-          />
+          <Stack direction={"row"} spacing={1} justifyContent="space-between">
+            <input
+              className="registration-input"
+              required
+              type="text"
+              placeholder="First Name"
+              name="firstName"
+              onChange={handleChange}
+            />
+            <input
+              className="registration-input"
+              required
+              type="text"
+              placeholder="Last Name"
+              name="lastName"
+              onChange={handleChange}
+            />
+          </Stack>
+          <Stack direction={"row"} spacing={1} justifyContent="space-between">
+            <input
+              className="registration-input"
+              required
+              type="password"
+              placeholder="Password"
+              name="password"
+              onChange={handleChange}
+            />
+            <input
+              className="registration-input"
+              required
+              type="password"
+              placeholder="Confirm Password"
+              name="confirmPassword"
+              onChange={handleChange}
+            />
+          </Stack>
+          <p className="registration-msg">{message}</p>
           <div className="radio">
             <p className="registration-radio-p">
               Are you a teacher or student?
@@ -116,7 +140,7 @@ const Register = () => {
               Terms of Service
             </Link>
           </span>
-          <button type="submit" className="sign-up-btn">
+          <button type="submit" className="sign-up-btn" onClick={submitData}>
             Create account
           </button>
           <button className="google-btn">
@@ -137,3 +161,20 @@ const Register = () => {
   );
 };
 export default Register;
+
+//check if password is valid (at least 8 characters and has at least one number)
+function passwordCheck(password){
+  if(password.length >= 8 && /[0-9]/.test(password))
+    return true
+  else
+    return false
+}
+
+//check if email is valid
+function emailCheck(email){
+  const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  if(emailRegex.test(email))
+    return true
+  else
+    return false
+}
