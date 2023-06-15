b93a22dce7b72dd02b004436d6a8eecb8dbdc9af
Fuadain <nicholausafleming@gmail.com>
2023-06-15 15:11:49 -0700

    added user type context to sidebar

--- src/sidebar/sidebar.js
+++ src/sidebar/sidebar.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import './sidebar.css';
 import { NavLink } from 'react-router-dom';
+import AxiosContext from '../components/axios-context';
 
 export default function Sidebar(){
-    const [isTeacher, identifyUser] = React.useState(false);
-    const [firstName, inputFirstName] = React.useState("Student")
-/* 
-USE URL that obtains info from SQL db
-axios.get('/login',{
-    name: name, (set state using this name)
-    profileType: teacher_student
-}) */
-    React.useEffect(() => {
-        isTeacher ? inputFirstName('Teacher') : inputFirstName('Student');
-      }, [isTeacher]);
-
+    const {userType} = React.useContext(AxiosContext)
+    
     function studentSidebar(){
         return (
             <ul className="sidebar-list-container">
-            <li><h2 id="sidebar-welcome">Welcome {firstName}!</h2></li>
+            <li><h2 id="sidebar-welcome">Welcome {userType}!</h2></li>
             <NavLink to="/dashboard" id="sidebar-profile" className="sidebar-option-link"><li className="sidebar-option">Home</li></NavLink>
             <NavLink to="/account" id="sidebar-profile" className="sidebar-option-link"><li className="sidebar-option">Account</li></NavLink>
             <NavLink to="/logout" id="sidebar-logout" className="sidebar-option-link"><li className="sidebar-option">Logout</li></NavLink>
@@ -29,7 +20,7 @@ axios.get('/login',{
     function teacherSidebar(){
         return (
             <ul className="sidebar-list-container">
-            <li><h2 id="sidebar-welcome">Welcome {firstName}!</h2></li>
+            <li><h2 id="sidebar-welcome">Welcome {userType}!</h2></li>
             <NavLink to="/dashboard" id="sidebar-profile" className="sidebar-option-link"><li className="sidebar-option">Home</li></NavLink>
             <NavLink to="/account" id="sidebar-profile" className="sidebar-option-link"><li className="sidebar-option">Account</li></NavLink>
             <NavLink to="/logout" id="sidebar-logout" className="sidebar-option-link"><li className="sidebar-option">Logout</li></NavLink>
@@ -38,7 +29,7 @@ axios.get('/login',{
     }
     return (
         <nav id="sidebar-container">
-        {isTeacher ? teacherSidebar() : studentSidebar()}               
+        {userType ? teacherSidebar() : studentSidebar()}               
         </nav>
     )
 }
