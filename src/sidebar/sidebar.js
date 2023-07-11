d7d38dea8a763728c80cafcb9291833b6c3c8c89
Lawrence-Clemente <lawrence.clemente.cohort225@gmail.com>
2023-07-10 17:12:28 -0700

    used react fontawesome

--- src/sidebar/sidebar.js
+++ src/sidebar/sidebar.js
@@ -3,6 +3,8 @@ import './sidebar.css';
 import { NavLink } from 'react-router-dom';
 import AxiosContext from '../components/axios-context';
 import LogoutContext from '../components/logout-context';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse, faUser, faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 
 export default function Sidebar(){
     const {userType} = React.useContext(AxiosContext)
@@ -18,19 +20,19 @@ export default function Sidebar(){
             </li>
             <li className="sidebar-option">
             <NavLink to="/dashboard" id="sidebar-profile" className="sidebar-option-link">
-                <i className="fa-solid fa-house"/> 
+                <FontAwesomeIcon icon={faHouse} className='fontawesome-icon'/>
                 <span>Home</span>
             </NavLink>
             </li>
             <li className="sidebar-option">
                 <NavLink to="/account" id="sidebar-profile" className="sidebar-option-link">
-                    <i className="fa-solid fa-user"/> 
+                    <FontAwesomeIcon icon={faUser} className='fontawesome-icon'/>
                     <span>Account</span>
                 </NavLink>
             </li>
             <li className="sidebar-option" id="sidebar-logout" onClick={logout}>
                 <div className="sidebar-option-link">
-                    <i className="fa-solid fa-arrow-right-from-bracket"/>
+                    <FontAwesomeIcon icon={faArrowRightFromBracket} className='fontawesome-icon'/>
                     <span>Logout</span>
                 </div>
             </li>
