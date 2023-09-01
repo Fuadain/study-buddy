4a9631331d0aa0ea58367308d6612c8a5969a7b1
Lawrence-Clemente <lawrence.clemente.cohort225@gmail.com>
2023-09-01 13:07:33 -0700

    login page is responsive

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -5,7 +5,7 @@ import{Stack, Typography, Button,
     InputLabel, IconButton, FormLabel, RadioGroup, FormHelperText,
 FormControlLabel, Radio, Divider } from '@mui/material';
 import{Visibility, VisibilityOff} from '@mui/icons-material';
-import axios from 'axios'
+import axios from 'axios';
 
 export default function Login(props){
 
@@ -47,31 +47,49 @@ export default function Login(props){
             sx={{
             width: '100%',
             height: '100vh',
-            alignItems:'center', 
-            justifyContent: 'center',
+            alignItems:{
+                        sm:'center'
+                        }, 
+            justifyContent: {
+                            sm:'center'
+                            },
             backgroundColor: '#00b3ff83'
         }}
         >
-            <Typography variant='h3' gutterBottom>
+            <Typography variant='h3' gutterBottom
+                sx={{ textAlign: 'center', padding:'20px 0'}}>
                 Login
             </Typography>
-            <form className='login-form' onSubmit={handleSubmit}>
+            <form className='login-form' onSubmit={handleSubmit} 
+            style={{ display:'flex', justifyContent:'center'}}>
+            {/*Login Container*/}
             <Stack
                 sx={{
-                    width: '60ch',
-                    height: '300px',
-                    backgroundColor: '#00B2FF',
+                    width:{ xs: '60%',
+                            sm:'60ch'
+                            },
+                    height:{ 
+                            xs:'400px',
+                            sm:'300px'
+                            },
+                    backgroundColor: 'white',
                     border: 1,
                     borderColor: 'black',
                     borderRadius: 2,
                     padding: 5,
-                    alignItems: 'center'
+                    justifyContent:{xs:'space-around'},
+                    alignItems: {sm:'center'}
                 }}
             >
                 
                 {/* Username Input Box */}
-                    <FormControl sx={{width: '50ch', 
-                                    margin:'0 0 1rem 2%'}}
+                    <FormControl sx={{
+                                    width: { 
+                                             xs:'100%',
+                                             sm:'50ch'
+                                            }, 
+                                    margin:'0 0 1rem 2%',
+                                    backgroundColor:'rgb(211,211,211)'}}
                                     >
                     <InputLabel htmlFor="component-outlined">Email</InputLabel>
                     <OutlinedInput
@@ -84,8 +102,13 @@ export default function Login(props){
                     </FormControl>
 
                     {/* Password Input Box */}
-                    <FormControl sx={{width: '50ch', 
-                                    margin:'0 0 1rem 2%'
+                    <FormControl sx={{
+                                    width: { 
+                                             xs:'100%',
+                                             sm:'50ch'
+                                            }, 
+                                    margin:'0 0 1rem 2%',
+                                    backgroundColor:'rgb(211,211,211)'
                                     }}
                                     variant="outlined">
                     <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
@@ -111,15 +134,33 @@ export default function Login(props){
                     />
                     </FormControl>
                     <Link to="/forgot-password">
-                        <Typography variant="overline" display="block" align="center" color="rgba(50,50,50,1)" gutterBottom>
+                        <Typography variant="overline" display="block" align="center" gutterBottom
+                            sx={{
+                                border:'0px solid red',
+                                color:'rgb(50,50,50)'}}>
                             Forgot Password? 
                         </Typography>
                     </Link>
     
-                    <Button sx={{width: '25ch', alignSelf: 'center', backgroundColor:'#00b3ff8', color: 'black'}} type="submit" variant="contained" size="medium">Login</Button>
+                    <Button sx={{
+                        width: '25ch', 
+                        alignSelf: 'center', 
+                        backgroundColor:'#1976d2', 
+                        color: 'white'
+                        }} 
+                    type="submit" variant="contained" size="medium">
+                        Login
+                    </Button>
              
             <Divider sx={{marginTop: 2, marginBottom: 2}} variant="middle" />
-            <Button sx={{color: 'white', backgroundColor: '#ef4443', width: '25ch', alignSelf: 'center'}} type="submit" variant="outlined" size="medium">Login with Google</Button>
+            <Button sx={{
+                        color: 'white', 
+                        backgroundColor: '#ef4443', 
+                        width: '25ch', alignSelf: 'center'
+                        }} 
+            type="submit" variant="outlined" size="medium">
+                Login with Google
+            </Button>
             </Stack></form>
         </Stack>
     )
