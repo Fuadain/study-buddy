a6e7e84cda5cdaab20ce5bcde3f93e269bc711ec
Lawrence-Clemente <lawrence.clemente.cohort225@gmail.com>
2023-09-22 01:35:50 -0700

    adjusted height for homepage on sideways phone res

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -3,13 +3,33 @@ import { Stack } from '@mui/material';
 import { Navbar, Body, Footer } from './';
 
 const Homepage = () => {
+    let smallPhoneSideHeight;
+    let mediumPhoneSideHeight;
+
+    //Small res sideways phones
+     if(window.innerWidth > 650 && window.innerWidth < 900 && window.innerHeight < 550)
+     {  
+        smallPhoneSideHeight = '300vh'
+    } else{ 
+        smallPhoneSideHeight = '100vh'
+    };
+
+
+    //for Samsung Galaxy A51/71 Sideways res
+    if(window.innerWidth === 914 && window.innerHeight === 412)
+     {  
+        mediumPhoneSideHeight = '150vh'
+    } else{ 
+        mediumPhoneSideHeight = '120vh'
+    };
+
     return (
         <Stack  
             sx={{
                 height: {
                         xs: '100vh',
-                        sm: '100vh',
-                        md: '120vh',
+                        sm: smallPhoneSideHeight,
+                        md: mediumPhoneSideHeight, 
                         lg: '100vh'
                         },
                 minWidth: '100%'
